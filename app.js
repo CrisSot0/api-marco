59e62fddca4a1cdb66cf3c7b78626f0963e2075b
CrisSot0 <612110440@utsalamanca.edu.mx>
2023-03-14 13:45:03 -0600

    Ya esta el buscador

--- app.js
+++ app.js
@@ -5,26 +5,37 @@ const fragment = document.createDocumentFragment()
 const selector = document.getElementById('selector')
 let tvseries = []
 
-btnLoad.addEventListener('click', () =>{
-    
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'eceb7882b1msh983c54bc02fb5c2p13ee3ajsn9e9bbe1a3761',
-            'X-RapidAPI-Host': 'watchmode.p.rapidapi.com'
-        }
-    };
-    
-    console.log('Serie: ',selector.value)
+const buscador = document.getElementById('inputText')
 
-    fetch(`https://watchmode.p.rapidapi.com/autocomplete-search/?search_value=${selector.value}&search_type=1`, options) //para buscar con 2 nombres %20
-        .then(response => response.json())
-        .then(response => {
-            tvseries = response.results
-            console.log(tvseries)
-            creaTarjetas()
-        })
-        .catch(err => console.log(err));
+
+
+function recargar () {
+    window.location.href = window.location.href;
+}
+
+btnLoad.addEventListener('click', () => {
+    console.log(buscador)
+
+    if(buscador.value){
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'eceb7882b1msh983c54bc02fb5c2p13ee3ajsn9e9bbe1a3761',
+                'X-RapidAPI-Host': 'watchmode.p.rapidapi.com'
+            }
+        };
+        
+        //console.log('Serie: ',selector.value)
+    
+        fetch(`https://watchmode.p.rapidapi.com/autocomplete-search/?search_value=${buscador.value}&search_type=1`, options) //para buscar con 2 nombres %20
+            .then(response => response.json())
+            .then(response => {
+                tvseries = response.results
+                console.log(tvseries)
+                creaTarjetas()
+            })
+            .catch(err => console.log(err));
+    }
 })
 
 const creaTarjetas = () => {
